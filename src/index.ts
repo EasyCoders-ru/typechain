387c5d6d640fa0bce651517567ccccfaa0eac878
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-18 23:00:38 +0300

    0.6 Интерфейсы в TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-const sayHi = (name: string, age: number, gender: string): string =>
-  `Привет, ${name}. Тебе ${age} лет и ты ${gender}!!!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
 
-console.log(sayHi("Антон", 28, "мужчина"));
+const person = {
+  name: "Антон",
+  age: 28,
+  gender: "мужчина",
+};
+
+const sayHi = (person: Human): string =>
+  `Привет, ${person.name}. Тебе ${person.age} лет и ты ${person.gender}!!!`;
+
+console.log(sayHi(person));
 
 export {};

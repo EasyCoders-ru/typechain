6a0e00f266fea22236f1107f086b275ac4b7d0d4
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-18 22:06:40 +0300

    0.5 Типы в TypeScript

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const sayHi = (name: string, age: number, gender: string): string =>
+  `Привет, ${name}. Тебе ${age} лет и ты ${gender}!!!`;
+
+console.log(sayHi("Антон", 28, "мужчина"));
+
+export {};
